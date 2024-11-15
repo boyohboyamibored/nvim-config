74e2c2e5eac49260f7ac6d7480227a454d9eb569
boyohboyamibored <hermit@cabin.localdomain>
2024-11-15 16:46:19 +0000

    Added few things to neo-tree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -10,6 +10,21 @@ return {
 	config = function()
 		require("neo-tree").setup({
 			close_if_last_window = true,
+			window = {
+				mappings = {
+					["l"] = "open",
+					["h"] = "close_node",
+					["<space>"] = "close_node",
+					["Y"] = {
+						function(state)
+							local node = state.tree:get_node()
+							local path = node:get_id()
+							vim.fn.setreg("+", path, "c")
+						end,
+						desc = "Copy Path to Clipboard",
+					},
+				},
+			},
 		})
 		-- automatically open tree if no file specified
 		-- if vim.fn.argc(-1) == 0 then
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,36 +1,36 @@
 return {
-    {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.8",
-        dependencies = { "nvim-lua/plenary.nvim" },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
 
-        config = function()
-            local builtin = require("telescope.builtin")
-            vim.keymap.set("n", "<C-f>", builtin.oldfiles, { desc = "Telescope recent files" })
-            vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Telescope recent files" })
-            vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-            vim.keymap.set("n", "<leader>fo", builtin.buffers, { desc = "Telescope open files" })
-            vim.keymap.set("n", "<leader>ft", builtin.colorscheme, { desc = "Colorscheme picker" })
-            vim.keymap.set("n", "<leader>fc", function()
-                builtin.find_files({
-                    cwd = "~/.config/nvim",
-                    hidden = false, -- don't show hidden
-                })
-            end, { desc = "Telescope nvim config" })
-        end,
-    },
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
+		keys = function()
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<C-f>", builtin.oldfiles, { desc = "Telescope recent files" })
+			vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Telescope recent files" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+			vim.keymap.set("n", "<leader>fo", builtin.buffers, { desc = "Telescope open files" })
+			vim.keymap.set("n", "<leader>ft", builtin.colorscheme, { desc = "Colorscheme picker" })
+			vim.keymap.set("n", "<leader>fc", function()
+				builtin.find_files({
+					cwd = "~/.config/nvim",
+					hidden = false, -- don't show hidden
+				})
+			end, { desc = "Telescope nvim config" })
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
 
-        config = function()
-            require("telescope").setup({
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({}),
-                        require("telescope").load_extension("ui-select"),
-                    },
-                },
-            })
-        end,
-    },
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+						require("telescope").load_extension("ui-select"),
+					},
+				},
+			})
+		end,
+	},
 }
